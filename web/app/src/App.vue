3995b2764400329f1658f010f0f9d811e8304e15
gODealOAple <maksim.math@gmail.com>
2021-12-04 13:45:37 +0500

    Add base page

--- web/app/src/App.vue
+++ web/app/src/App.vue
@@ -1,22 +1,63 @@
 <template>
-  <div id="app"></div>
+  <header class="header">
+    <span class="header_name">РЖД Поездки</span>
+    <div class="header_nav">
+      <router-link to="/">Поездка</router-link>
+    </div>
+  </header>
+  <router-view />
+  <div class="footer">
+    <div class="contacts">Контакты: Асанов Магаз Оразкимович</div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  components: {
-  }
-}
+import "./assets/css/normalize.css";
+
+export default {};
 </script>
 
-<style>
+<style lang="scss">
 #app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-size: 1.3vw;
+}
+
+.header {
+  background: black;
+
+  &_name {
+    color: red;
+    margin: 0;
+  }
+
+  &_nav {
+    padding: 15px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+}
+
+.footer {
+  width: 100%;
+}
+
+.contacts {
+  padding: 10px;
+  background: #39b54a;
 }
 </style>
---
+++ web/app/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <div class="content">Content</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  flex-grow: 1;
+}
+</style>
