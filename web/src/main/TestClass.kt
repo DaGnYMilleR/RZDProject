3ab4b56b5ef0523f7ce65b41bc4d84dbcc0b6ad3
DaGnYMilleR <danil.lunyow@mail.ru>
2021-12-04 20:39:09 +0500

    update gradle version and add swagger-ui

--- web/src/main/TestClass.kt
+++ web/src/main/TestClass.kt
@@ -1,9 +1,18 @@
 package main
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 
 @SpringBootApplication
 class DemoApplication
@@ -13,8 +22,11 @@ fun main(args: Array<String>) {
 }
 
 @RestController
+@RequestMapping("/api")
+@Api(description = "Тестовый контроллер")
 class MessageResource {
     @GetMapping
+    @ApiOperation("Тестовый метод")
     fun index(): List<Message> = listOf(
         Message("1", "Hello!"),
         Message("2", "Bonjour!"),
@@ -23,3 +35,16 @@ class MessageResource {
 }
 
 data class Message(val id: String?, val text: String)
+
+/*@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun api(): Docket =
+        Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+
+}*/
