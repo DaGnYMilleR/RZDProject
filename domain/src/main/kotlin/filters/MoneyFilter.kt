c94ba30580d3497de9c54da144d6c45f0b5e077a
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2021-12-06 20:54:30 +0500

    "Add functions filter money"

--- domain/src/main/kotlin/filters/MoneyFilter.kt
+++ domain/src/main/kotlin/filters/MoneyFilter.kt
@@ -4,6 +4,12 @@ import models.Journey
 
 class MoneyFilter : IFilter {
     override fun filter(journeys: List<Journey>): List<Journey> {
-        TODO("Not yet implemented")
+        journeys.stream().forEach { x -> x.hotels.stream().filter { y -> y.price + x.ticket.cost <= 20000 } }
+        return journeys
+    }
+
+    fun filter(journeys: List<Journey>, cost: Double): List<Journey> {
+        journeys.stream().forEach{ x -> x.hotels.stream().filter { y -> y.price + x.ticket.cost <= cost } }
+        return journeys
     }
 }
