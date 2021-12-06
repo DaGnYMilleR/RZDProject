4c94b5fb37d9a6332d063e0e69dbf8cc66c9b89e
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2021-12-06 21:55:10 +0500

    MoneyFilter refactor

--- domain/src/main/kotlin/filters/MoneyFilter.kt
+++ domain/src/main/kotlin/filters/MoneyFilter.kt
@@ -1,15 +1,11 @@
 package filters
 
 import models.Journey
+import IParameters
 
 class MoneyFilter : IFilter {
-    override fun filter(journeys: List<Journey>): List<Journey> {
-        journeys.stream().forEach { x -> x.hotels.stream().filter { y -> y.price + x.ticket.cost <= 20000 } }
-        return journeys
-    }
-
-    fun filter(journeys: List<Journey>, cost: Double): List<Journey> {
-        journeys.stream().forEach{ x -> x.hotels.stream().filter { y -> y.price + x.ticket.cost <= cost } }
+    override fun filter(journeys: List<Journey>, parameters: IParameters): List<Journey> {
+        journeys.forEach { journey -> journey.hotels.stream().filter { hotel -> hotel.price + journey.ticket.cost <= parameters.money } }
         return journeys
     }
 }
