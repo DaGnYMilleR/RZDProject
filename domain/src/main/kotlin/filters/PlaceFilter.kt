6b34d0973897fa164d82a643748f1d3ddaea54b0
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-12-06 22:26:33 +0500

    fix Filters implementations

--- domain/src/main/kotlin/filters/PlaceFilter.kt
+++ domain/src/main/kotlin/filters/PlaceFilter.kt
@@ -1,9 +1,10 @@
 package filters
 
+import IParameters
 import models.Journey
 
 class PlaceFilter : IFilter {
-    override fun filter(journeys: List<Journey>): List<Journey> {
+    override fun filter(journeys: List<Journey>, parameters: IParameters): List<Journey> {
         TODO("Not yet implemented")
     }
 }
