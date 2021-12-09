4d8e975f98886cba70fd11d57feb9bce877f48c7
gODealOAple <maksim.math@gmail.com>
2021-12-09 14:56:28 +0500

    Update main page

--- web/app/src/App.vue
+++ web/app/src/App.vue
@@ -1,29 +1,57 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-  </div>
+  <header class="header">
+    <span class="header_name">РЖД Поездки</span>
+    <div class="header_nav">
+      <router-link to="/">Поездка</router-link>
+    </div>
+  </header>
   <router-view />
+  <div class="footer">
+    <div class="contacts">Контакты: Асанов Магаз Оразкимович</div>
+  </div>
 </template>
 
 <style lang="scss">
 #app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 1.3vw;
 }
 
-#nav {
-  padding: 30px;
+.header {
+  background: black;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  &_name {
+    color: red;
+    margin: 0;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  &_nav {
+    padding: 15px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
+}
+
+.footer {
+  width: 100%;
+}
+
+.contacts {
+  padding: 10px;
+  background: #39b54a;
 }
 </style>
