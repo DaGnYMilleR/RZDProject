502ec939ba29a438e8308972f5d4b002d131c4b0
gODealOAple <maksim.math@gmail.com>
2021-12-12 01:09:59 +0500

    Tiny refactoring

--- web/app/src/models/JourneyParametersRequest.js
+++ web/app/src/models/JourneyParametersRequest.js
@@ -12,31 +12,3 @@ export class JourneyParametersRequest {
   }
 }
 
-export class HotelResponse {
-  id;
-  name;
-  address;
-  price;
-  rating;
-  imageUrl;
-  /**
-   * @param {Number} id
-   * @param {String} name
-   * @param {Object} address
-   * @param {Number} price
-   * @param {Number} rating
-   * @param {String} imageUrl
-   */
-  constructor(id, name, address, price, rating, imageUrl) {
-    this.name = name;
-    this.address = address;
-    this.price = price;
-    this.rating = rating;
-    this.imageUrl = imageUrl;
-    this.id = id;
-  }
-}
-HotelResponse.parse = (obj) => {
-  return new HotelResponse(obj.id, obj.name, obj.address, obj.price, obj.rating, obj.imageUrl);
-}
-
--- web/app/src/router/index.js
+++ web/app/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import SearchJourneys from "../views/SearchJourneys";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'SearchJourneys',
+    component: SearchJourneys
   },
 ]
 
