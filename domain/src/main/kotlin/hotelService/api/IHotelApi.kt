ccc7d9f770017c73095f8b464223ce4fd508e49d
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-12-12 23:30:08 +0500

    remove @Component

--- domain/src/main/kotlin/hotelService/api/IHotelApi.kt
+++ domain/src/main/kotlin/hotelService/api/IHotelApi.kt
@@ -2,9 +2,8 @@ package hotelService.api
 
 import models.City
 import models.DateSegment
-import org.springframework.stereotype.Component
 
-@Component
+
 interface IHotelApi {
     fun makeRequest(destinationCity: City, journeyDuration: DateSegment): Array<HotelResponse>
 }
