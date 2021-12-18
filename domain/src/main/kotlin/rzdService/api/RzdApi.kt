d48c4380da0812796765a447c8abfdb397b42ed7
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-12-19 01:06:38 +0500

    Tickets: add urls to tutu-site

--- domain/src/main/kotlin/rzdService/api/RzdApi.kt
+++ domain/src/main/kotlin/rzdService/api/RzdApi.kt
@@ -5,6 +5,7 @@ import models.City
 import models.DateSegment
 import models.Ticket
 import models.TravellingTime
+import java.time.LocalDate
 import rzdService.api.IRzdApi as IRzdApi1
 
 class RzdApi(private val httpService: HttpService) : IRzdApi1 {
@@ -20,6 +21,7 @@ class RzdApi(private val httpService: HttpService) : IRzdApi1 {
                     val urlFrom = "https://suggest.travelpayouts.com/search?service=" +
                             "tutu_trains&term=${idTo}&term2=${idFrom}&date=${time2}"
                     val ticketsTo = httpService.getResponse<RzdResponse>(urlTo)?.trips
+
                     val ticketsFrom = httpService.getResponse<RzdResponse>(urlFrom)?.trips
                     if (ticketsTo != null && res.isEmpty() && ticketsTo.isNotEmpty() && ticketsFrom != null && ticketsFrom.isNotEmpty()) {
                         val price2 = ticketsFrom.map { it.categories.first().price }.first()
@@ -31,14 +33,28 @@ class RzdApi(private val httpService: HttpService) : IRzdApi1 {
                                 TravellingTime(
                                     DateSegment(time, time.plusDays(1)),
                                     DateSegment(time2.minusDays(1), time2)
-                                )
+                                ),
+                                createUrlToBuyTicket(idTo, idFrom, time, it.numberForUrl),
+                                createUrlToBuyTicket(idFrom, idTo, time2, ticketsFrom.first().numberForUrl)
                             )
                         }
                         return@first
-    }
                     }
                 }
             }
+        }
         return res
     }
+
+    private fun createUrlToBuyTicket(idTo: Int, idFrom: Int, date: LocalDate, numberForUrl: String): String =
+        "https://www.tutu.ru/poezda/order/?dep_st=$idFrom&arr_st=$idTo&tn=$numberForUrl%3D&date=$date"
+
+
+    private fun getResponse(idTo: Int, idFrom: Int, date: LocalDate): List<Trip>? {
+        val url = getUlrWithParameters(idFrom, idTo, date)
+        return httpService.getResponse<RzdResponse>(url)?.trips
+    }
+
+    private fun getUlrWithParameters(idTo: Int, idFrom: Int, date: LocalDate): String =
+        "https://suggest.travelpayouts.com/search?service=tutu_trains&term=${idTo}&term2=${idFrom}&date=${date}"
 }
