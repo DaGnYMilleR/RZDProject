b8b02978eb6922acfb3de760cb12ad8db13252fc
gODealOAple <maksim.math@gmail.com>
2021-12-19 21:00:17 +0500

    Update dto-models to sync with server-dto

--- web/app/src/models/entity/City.js
+++ web/app/src/models/entity/City.js
@@ -5,16 +5,23 @@ export class City {
    * @param {String} name
    * @param {Tag[]} tags
    * @param {Number[]} stationsIds
+   * @param {String} imageUrl
    */
-  constructor(name, tags, stationsIds) {
+  constructor(name, tags, stationsIds, imageUrl) {
     this.name = name;
     this.tags = tags;
     this.stationsIds = stationsIds;
+    this.imageUrl = imageUrl;
   }
 }
 
 City.fromObject = (obj) => {
-  return new City(obj.name, obj.tags.map(Tag.fromObject), obj.stationsId);
+  return new City(
+    obj.name,
+    obj.tags.map(Tag.fromObject),
+    obj.stationsIds,
+    obj.imageUrl
+  );
 };
 
-City.Null = new City("", [], []);
+City.Null = new City("", [], [], "");
--- web/app/src/models/entity/Ticket.js
+++ web/app/src/models/entity/Ticket.js
@@ -7,12 +7,16 @@ export class Ticket {
    * @param {City} cityTo
    * @param {Number} cost
    * @param {TravellingTime} travellingTime
+   * @param {String} urlTo
+   * @param {String} urlFrom
    */
-  constructor(cityFrom, cityTo, cost, travellingTime) {
+  constructor(cityFrom, cityTo, cost, travellingTime, urlTo, urlFrom) {
     this.cityFrom = cityFrom;
     this.cityTo = cityTo;
     this.cost = cost;
     this.travellingTime = travellingTime;
+    this.urlTo = urlTo;
+    this.urlFrom = urlFrom;
   }
 }
 
@@ -21,8 +25,10 @@ Ticket.fromObject = (obj) => {
     City.fromObject(obj.cityFrom),
     City.fromObject(obj.cityTo),
     obj.cost,
-    TravellingTime.fromObject(obj.travellingTime)
+    TravellingTime.fromObject(obj.travellingTime),
+    obj.urlTo,
+    obj.urlFrom
   );
 };
 
-Ticket.Null = new Ticket(City.Null, City.Null, 0, TravellingTime.Null);
+Ticket.Null = new Ticket(City.Null, City.Null, 0, TravellingTime.Null, "", "");
