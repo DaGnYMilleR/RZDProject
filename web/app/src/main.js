fe8856d4f75a75b69f9378961da82aabf89ea706
gODealOAple <maksim.math@gmail.com>
2021-12-28 01:49:42 +0500

    Add Vuex

--- web/app/src/main.js
+++ web/app/src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vuex from "vuex";
 import vuetify from "./plugins/vuetify";
 import "./plugins/vuetify-money.js";
 
@@ -8,6 +9,7 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  Vuex,
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
