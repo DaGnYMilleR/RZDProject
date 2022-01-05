590ff68edba104f501c719a3cbdff7f3d78a4032
DaGnYMilleR <danil.lunyow@mail.ru>
2022-01-05 22:46:41 +0500

    hotels service refactoring

--- domain/src/main/kotlin/hotelService/HotelService.kt
+++ domain/src/main/kotlin/hotelService/HotelService.kt
@@ -7,9 +7,10 @@ import models.hotel.Hotel
 
 class HotelService(private val hotelApi: IHotelApi, private val imageService: IHotelImageService) : IHotelService {
     private val hotelsCount = 5
+    private val maxHotelsFromApi = 500
 
     override fun getHotels(params: HotelServiceParams): List<Hotel> {
-        val response = getHotels(hotelsCount, params)
+        val response = getHotelsFromApi(params)
 
         if(response.isEmpty())
             return emptyList()
@@ -22,23 +23,9 @@ class HotelService(private val hotelApi: IHotelApi, private val imageService: IH
             }
     }
 
-    private fun getHotels(count: Int, params: HotelServiceParams) : List<HotelResponse>{
-        val step = 100
-        var elementsCount = step
-        val result = mutableListOf<HotelResponse>()
-
-        for (i in 1..5){
-            val responses = hotelApi.makeRequest(params.city, params.journeyDuration, elementsCount)
-
-            for(response in responses.drop(elementsCount - step)){
-                if(response.priceAvg <= params.budget){
-                    result.add(response)
-                }
-                if(result.size == count)
-                    return result
-            }
-            elementsCount += step
-        }
-        return result
-    }
+    private fun getHotelsFromApi(params: HotelServiceParams) : List<HotelResponse>
+        = hotelApi.makeRequest(params.city, params.journeyDuration, maxHotelsFromApi)
+            .filter { it.priceAvg <= params.budget }
+            .sortedBy { it.priceAvg }
+            .take(hotelsCount)
 }
